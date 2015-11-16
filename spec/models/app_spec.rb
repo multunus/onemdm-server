51881fa3fbc054102c76824ae37c6f6bccfa4a1e
leenasn <leena.sn@multunus.com>
2015-11-16 19:04:52 +0530

    Add GCM push for pushing OneMDM app to devices
    - Add dashboard showing the status of installations
    - Add default status as 'pushed' for installation
    - Add has_many relations for installations for app and device models
    - Add logic to read the APP URL from config file

---
+++ spec/models/app_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe App, type: :model do
+
+  let!(:app) { create(:app) }
+
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :package_name } 
+
+  describe "APK URL" do
+    it "Default app" do
+      app = create(:app)
+      expect(app.apk_url).to eql(DEFAULT_APP_APK_URL)
+    end
+    it "Non Default app" do
+      app = App.create(name: "Non MDM", package_name: "com.nonmdm")
+      expect(app.apk_url).to eql("")
+    end
+    
+  end
+end
