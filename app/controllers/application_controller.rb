129f2807239fdc6ba6a19e838218a3f4d72cfc9a
Leena <leenasn@gmail.com>
2016-02-24 23:46:15 +0530

    Remove the duplicate method definition

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  def authenticate_device
-    authenticate_with_token || render_unauthorized
-  end
-
+  # def authenticate_device
+  #   authenticate_with_token || render_unauthorized
+  # end
+  #
   def authenticate_device
     authenticate_or_request_with_http_token do |token, options|
       @device = Device.find_by(access_token: token)
